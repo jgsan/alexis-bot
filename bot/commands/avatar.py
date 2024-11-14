8188eb6bf26580fde6d7b24515e1fea872ca1553
makzk <contacto@jgutierrez.cl>
2024-11-14 12:32:01 -0300

    Fix avatar

--- bot/commands/avatar.py
+++ bot/commands/avatar.py
@@ -13,4 +13,4 @@ async def avatar(interaction: discord.Interaction, member: Optional[discord.Memb
     ext_url = str(member.display_avatar.with_static_format('png'))
     text = 'Avatar' if bool(member.display_avatar.url) else 'Avatar predeterminado'
     embed = img_embed(str(member.display_avatar.url), text, '[Link externo]({})'.format(ext_url))
-    await interaction.response.send_message(embed)
+    await interaction.response.send_message(embed=embed)
